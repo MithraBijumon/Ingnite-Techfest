697861cf1772693c06746fe70fd80f6b7f19af85
icodeforlife24 <shiviutkarsh@gmail.com>
2025-08-12 19:26:55 +0530

    Initialize Next.js project with basic structure
    Set up a new Next.js app with initial configuration, including global styles, a navbar component, project metadata, and Tailwind CSS integration. Added essential config files, a placeholder home page, and project assets such as the logo and favicon.

---
+++ igniteportal/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+      <div class="absolute top-0 w-screen h-20 z-[-2] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] flex items-center">
+        <img src="/assets/logo.png" alt="Ignite Techfest Logo" className='invert h-15 m-8' />
+      </div>
+
+
+  )
+}
+
+export default navbar
