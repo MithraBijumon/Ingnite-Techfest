51792fea56cb16cb95f4d73ea296c17cd5db13c7
icodeforlife24 <shiviutkarsh@gmail.com>
2025-08-13 14:59:04 +0530

    Update homepage layout and add placeholder image
    Fixed import case for Incentive component in layout.js. Improved homepage styling, increased heading sizes, and added a placeholder image to the main page. Updated navbar z-index for proper stacking. Added placeholder.png to public assets.

--- igniteportal/app/page.js
+++ igniteportal/app/page.js
@@ -3,13 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div className="flex  items-center justify-center min-h-screen text-white">
-      <div class="flex flex-col">
-        <h1 className="text-7xl font-bold mb-4">
+    <div className="flex  items-center justify-around min-h-screen text-white">
+      <div className="flex flex-col m-10">
+        <h1 className="text-9xl font-bold mb-4">
           iGNiTE
         </h1>
-        <h3>tagline</h3>
+        <h3 className="text-2xl">tagline</h3>
       </div>
+      <img src="/assets/placeholder.png" alt="Ignite Techfest Logo" className='m-10 h-80' />
       </div>
     </>
 
