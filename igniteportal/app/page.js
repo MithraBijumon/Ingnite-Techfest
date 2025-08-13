8fc6012a0b15eb49d223a0cc69a46f86456faa39
icodeforlife24 <shiviutkarsh@gmail.com>
2025-08-13 20:43:45 +0530

    Update homepage layout and Flipcard styling
    Refactored the homepage layout for improved alignment and button styling, updated the call-to-action text, and enhanced the Flipcard back side with a gradient background. Also removed the unused placeholder.png image from assets.

--- igniteportal/app/page.js
+++ igniteportal/app/page.js
@@ -1,22 +1,25 @@
 import Image from "next/image";
+import about from "@/components/About";
+import Incentive from "@/components/Incentive";
+import ContactSection from "@/components/Contact";
 
 export default function Home() {
   return (
     <>
-    <div className="flex  items-center justify-around min-h-screen text-white">
-      <div className="flex flex-col items-center text-center">
-        <h1 className="text-9xl font-bold">
-          iGNiTE
-        </h1>
-        <h3 className="text-2xl">tagline</h3>
-        <button className="relative inline-flex items-center justify-center p-[2px] text-[19px] mt-4 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-2 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
-          <span className="relative px-3 py-1.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
-            Get Started!!
-          </span>
-        </button>
+      <div className="flex  items-center justify-around min-h-screen text-white">
+        <div className="flex flex-col items-start justify-center gap-4">
+          <h1 className="text-9xl font-bold">
+            iGNiTE
+          </h1>
+          <h3 className="text-2xl">tagline</h3>
+          <button className="relative inline-flex items-center  justify-center p-0.5 text-[19px] mx-auto overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+              Register Now!!
+            </span>
+          </button>
+        </div>
+        <img src="/assets/placeholder.png" alt="Ignite Techfest Logo" className='m-10 h-80' />
       </div>
-      <img src="/assets/placeholder.png" alt="Ignite Techfest Logo" className='m-10 h-80' />
-    </div>
     </>
 
   );
