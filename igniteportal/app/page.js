df8788c65d124e9d1ee1618f925345c5aa569aa8
icodeforlife24 <shiviutkarsh@gmail.com>
2025-08-19 20:48:30 +0530

    Create page.js
    Added a redirect to home if someone enters URL directly

---
+++ igniteportal/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { redirect } from "next/navigation";
+
+const page = () => {
+    redirect("/home");
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
